c0b3d8e3c0fdbb2a4b8c9109c1e8ba3dfa1efba6
Yahuri <85915815+Yahuri@users.noreply.github.com>
2025-09-10 23:08:28 -0700

    commission and display
    update commissions info and img display settings. bugged modal

--- pages/jss-portfolio.js
+++ pages/jss-portfolio.js
@@ -32,6 +32,23 @@ function RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() { 
+	modal.style.display = "none";
+}
+
+var images = document.getElementsByTagName('content');
+var modalImg = document.getElementById("img01");
+var i;
+for (i = 0; i < images.length; i++) {
+	images[i].onclick = function() {
+		modal.style.display = "block";
+		modalImg.src = this.src;
+		modalImg.alt = this.alt;
+	}
+}
+
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
